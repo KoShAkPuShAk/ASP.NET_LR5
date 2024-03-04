a65436f16a6527f9810083d8c89b8f1204dd1fe2
KoShAkPuShAk <106887147+KoShAkPuShAk@users.noreply.github.com>
2024-03-04 18:58:02 +0200

    Add files via upload

---
+++ WebApplication6/WebApplication6/Program.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+
+namespace WebApplication6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+
+                try
+                {
+                    if (context.Request.Path == "/check-cookies")
+                    {
+                        var form = context.Request.Form;
+                        var name = form["name"];
+                        var cookiesLifespan = form["cookies-lifespan"];
+
+                        if (context.Request.Cookies.TryGetValue("name", out var existingName))
+                        {
+                            await context.Response.WriteAsync($"Hello {existingName}!");
+                        }
+                        else
+                        {
+                            context.Response.Cookies.Append("name", name, new CookieOptions
+                            {
+                                Expires = Convert.ToDateTime(cookiesLifespan)
+                            });
+
+                            await context.Response.WriteAsync("Hello World!");
+                        }
+                    }
+                    else
+                    {
+                        await context.Response.SendFileAsync("html/index.html");
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    var path = context.Request.Path;
+                    app.Logger.LogError($"LogError {path} | {ex.Message} | {ex.Data} | {ex.Source}");
+                }
+            });
+
+            app.Run();
+        }
+    }
+}
